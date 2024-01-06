b00a8ba9ab356604733de8a68d80f899a78eb4c5
Okoye-kelvin <121132060+Okoye-kelvin@users.noreply.github.com>
2024-01-06 23:29:46 +0100

    Delete src directory

--- src/main/java/com/example/employeemanagementlist/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagementlist.Repository;
-
-import com.example.employeemanagementlist.Domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
